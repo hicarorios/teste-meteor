742c5b7d95725aabbe517e0b09c968336d73f143
Hicaro Alfonso Rios <hicarorios@gmail.com>
2017-08-09 00:32:20 -0400

    Teste de aplicacao utilizando meteor

---
+++ client/recipes/Recipes.js
@@ -0,0 +1,18 @@
+Template.Recipes.onCreated(function() {
+    var self = this;
+    self.autorun(function() {
+        self.subscribe('recipes');
+    });
+});
+
+Template.Recipes.helpers({
+    recipes: () => {
+        return Recipes.find({});
+    }
+});
+
+Template.Recipes.events({
+    'click .new-recipe': () => {
+        Session.set('newRecipe', true);
+    }
+});
---
+++ collections/Recipes.js
@@ -0,0 +1,76 @@
+Recipes = new Mongo.Collection('recipes');
+
+Recipes.allow({
+    insert: function(userId, doc) {
+        return !!userId;
+    },
+    update: function(userId, doc) {
+        return !!userId;
+    }
+});
+
+Ingredient = new SimpleSchema({
+    name: {
+        type: String
+    },
+    amount: {
+        type: String
+    }
+});
+
+RecipeSchema = new SimpleSchema({
+    name:{
+        type: String,
+        label: "Name"
+    },
+    desc: {
+        type: String,
+        label: "Description"
+    },
+    ingredients: {
+        type:[Ingredient]
+    },
+    inMenu: {
+        type: Boolean,
+        defaultValue: false,
+        optional: true,
+        autoform: {
+            type: "hidden"
+        }
+    },
+    author: {
+        type: String,
+        label: "Author",
+        autoValue: function() {
+            return this.userId
+        },
+        autoform: {
+            type: "hidden"
+        }
+    },
+    createdAt: {
+        type: Date,
+        label: "created At",
+        autoValue: function() {
+            return new Date()
+        },
+        autoform: {
+            type: "hidden"
+        }
+    }
+});
+
+Meteor.methods({
+    toggleMenuItem: function(id, currentState) {
+        Recipes.update(id, {
+            $set: {
+                inMenu: !currentState
+            }
+        });
+    },
+    deleteRecipe: function(id) {
+        Recipes.remove(id);
+    }
+});
+
+Recipes.attachSchema(RecipeSchema);
